a3cdee7be7946b6c58924d2245ce75370da1e7fb
Imran <imranmansuri038@gmail.com>
2023-02-25 23:00:58 +0530

    world section added

--- sections/World.jsx
+++ sections/World.jsx
@@ -1,9 +1,60 @@
-'use client';
+"use client";
+import { motion } from "framer-motion";
 
-const World = () => (
-  <section>
-    World section
-  </section>
-);
+import styles from "../styles";
+import { TitleText, TypingText } from "../components";
+import { fadeIn, staggerContainer } from "../utils/motion";
+import { data } from "autoprefixer";
+
+const World = () => {
+  const data = [
+    { src: "people-01.png", position: "bottom-20 right-20" },
+    { src: "people-02.png", position: "top-10 left-20" },
+    { src: "people-03.png", position: "top-1/2 left-[45%]" },
+  ];
+
+  return (
+    <section className={`${styles.paddings} relative z-10`}>
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{ once: false, amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto flex flex-col`}
+      >
+        <TypingText title="| People on the World" textStyles="text-center" />
+        <TitleText
+          title={
+            <>
+              Track friends around you and invite them to play together in the
+              same world
+            </>
+          }
+          textStyles="text-center"
+        />
+
+        <motion.div
+          variants={fadeIn("up", "tween", 0.3, 1)}
+          className="relative mt-[68px] flex w-full h-[550px]"
+        >
+          <img
+            src="/map.png"
+            alt="map"
+            className="w-full h-full object-cover"
+          />
+
+          {data.map((person, index) => (
+            <div
+              key={index}
+              className={`absolute ${person.position} w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]`}
+            >
+              <img src={person.src} alt="people" className="w-full h-full" />
+            </div>
+          ))}
+        </motion.div>
+      </motion.div>
+    </section>
+  );
+};
 
 export default World;
