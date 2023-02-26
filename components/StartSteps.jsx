a2bfaf12c4e2dd5b45ed00d7780cfc2eea6ee724
Imran <imranmansuri038@gmail.com>
2023-02-26 19:52:58 +0530

    files added again

---
+++ components/StartSteps.jsx
@@ -0,0 +1,22 @@
+<<<<<<< HEAD
+import styles from '../styles';
+=======
+import styles from "../styles";
+>>>>>>> 91c560f5af167bf3be0ffb2a1226b89279e1ec27
+
+const StartSteps = ({ number, text }) => (
+  <div className={`${styles.flexCenter} flex-row`}>
+    <div
+      className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323F5D]`}
+    >
+      <p className="font-bold text-[20px] text-white">
+        {number}
+      </p>
+    </div>
+    <p className="flex-1 ml-[30px] font-normal text-[18px] text-[#B0B0B0] leading-[32.4px]">
+      {text}
+    </p>
+  </div>
+);
+
+export default StartSteps;
